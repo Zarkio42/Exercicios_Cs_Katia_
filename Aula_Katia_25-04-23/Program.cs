1e403554594ad243139e0d4f396206d174b59490
Curso ADS2 <curso.ads2@academico.local>
2023-04-25 22:30:34 -0300

    commit fatorial

---
+++ Aula_Katia_25-04-23/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_Katia_25_04_23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //-------------------------------
+            // EXERCICIO DE FATORIAL
+            //-------------------------------
+            /*
+            int fat, i, calc_fat;
+
+            Console.WriteLine("Digite o valor do fatorial que você deseja resolver: ");
+            int.TryParse(Console.ReadLine(), out fat);
+
+            i = fat -1;
+            calc_fat = fat;  
+
+            while (i >= 1)
+            {
+                calc_fat = calc_fat * i;
+                i--;
+            }
+            Console.WriteLine($"{fat}! = {calc_fat}");
+            Console.ReadKey();
+
+            */
+
+        }
+    }
+}
