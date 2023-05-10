caeb3f73a7f1465fcebc39aab05eda989a3d8458
Curso ADS2 <curso.ads2@academico.local>
2023-05-09 22:08:35 -0300

    exercicio dos vetores

--- Aula_Katia-09-05-23/Aula_Katia-09-05-23/Program.cs
+++ Aula_Katia-09-05-23/Aula_Katia-09-05-23/Program.cs
@@ -10,6 +10,7 @@ namespace Aula_Katia_09_05_23
     {
         static void Main(string[] args)
         {
+            /*
 
             //Exercícios da Katia sobre adicionar dados em um vetor e resgatar eles!
             //----------------------------------------------------------------------
@@ -39,6 +40,70 @@ namespace Aula_Katia_09_05_23
             Console.WriteLine($"Média de todas as notas: {soma/nota.Length}");
 
             Console.ReadKey();
+
+            
+
+            //Ta faltando um pedaço do ex, mas o interessante é o metodo Next
+
+            //atribui elementos digitados pelo usuario no vetor
+
+            for (i = 0; i < qtde; i++)
+            {
+                Console.WriteLine("Informe o nome do aluno: ");
+                nome[i] = Console.ReadLine();
+
+                Console.WriteLine("Informe a nota");
+                nota[i] = ConvertToDouble(Console.ReadLine());
+                tf[i] = random.Next(0, 10);
+
+            }
+
+            for (i = 0; i < qtde; i++)
+            {
+                Console.Write($"\n {nome[i]} - Avaliação: {nota[i].ToString("n2")}");
+                Console.Write($" - TF: {tf[i].ToString("n2")}");
+                soma = (tf[i] * 3 + nota[i] * 7) / 10;
+                Console.Write(" - Média = " + soma.ToString("n2");
+            }
+            
+            Console.ReadKey();
+
+            
+
+            //---------------------------------------------------------------------------------
+            //Fazer um programa que recebe 5 números no primeiro vetor e 5 números no segundo.
+            //Armazene em um terceiro vetor os números intercalados dos dois primeiros vetores.
+            //---------------------------------------------------------------------------------
+
+            int[] vetor1 = new int[5];
+            int[] vetor2 = new int[5];
+            int[] vetor3 = new int[10];
+
+            //inserir dados no primeiro e no segundo vetor
+            for (int i = 0; i < 5; i++)
+            {
+                vetor1[i] = Convert.ToInt32(Console.ReadLine());
+                vetor2[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            //inserir os dados do 1º e do 2º vetor no 3º
+
+            for (int i = 0; i < 5; i++)
+            {
+                
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Vetor 1: {vetor1[i]}\nVetor 2: {vetor2[i]}\nVetor 3: {vetor3[i]}");
+
+
+            }
+
+            Console.ReadKey();
+            */
+
+
         }
     }
 }
