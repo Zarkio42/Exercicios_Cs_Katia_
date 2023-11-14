c30c82765baa9ae97ab117c9319fdb9615906872
Curso ADS2 <curso.ads2@academico.local>
2023-11-13 22:31:25 -0300

    subindo projeto pousada

---
+++ Projetos para Entrega/WF-Projeto01-Katia-RA1121036/Hospede.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WF_Projeto01_Katia_RA1121036
+{
+    abstract class Hospede
+    {
+        string Nome { get; set; }
+        int Cpf { get; set; }
+        int Telefone { get; set; }
+
+        public abstract double Calcular();
+
+    }
+}
---
+++ Projetos para Entrega/WF-Projeto01-Katia-RA1121036/Reserva.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WF_Projeto01_Katia_RA1121036
+{
+    internal class Reserva : Hospede
+    {
+        public double ValorTotalReserva { get; set; }
+        public DateTime DataCheckIn { get; set; }
+        public DateTime DataCheckOut { get; set; }
+
+        public bool ReservaEfetivada { get; set; }
+        public bool AtividadesExtra { get; set; }
+
+        public override double Calcular()
+        {
+            return ValorTotalReserva;
+        }
+    }
+}
